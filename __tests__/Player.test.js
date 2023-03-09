6089a9a4c2a129057053c4da14810c38b857613f
Amy McCabe <mcca0168@umn.edu>
2023-03-09 13:57:18 -0600

    getStats and getInventory methods added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,3 @@
-const { default: JestHasteMap } = require('jest-haste-map');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
@@ -12,4 +11,23 @@ test('creates a player object', () => {
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test('gets player stats as an object', () => {
+  const player = new Player('Amy');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns falss', () => {
+  const player = new Player('Amy');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
